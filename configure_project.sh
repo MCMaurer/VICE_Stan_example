ad104a1ad08f338b539d3a99f73a0ee7dea13a58
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2021-08-07 09:18:12 -0700

    Update configure_project.sh

--- configure_project.sh
+++ configure_project.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git config user.email "culshawmaurer@email.arizona.edu"
-git config user.name "Michael Culshaw-Maurer"
-
 git clone https://github.com/MCMaurer/VICE_Stan_example.git
 
-xdg-open VICE_Stan_example/VICE_Stan_example.Rproj
+cd VICE_Stan_example
+
+git config user.email "culshawmaurer@email.arizona.edu"
+git config user.name "Michael Culshaw-Maurer"
