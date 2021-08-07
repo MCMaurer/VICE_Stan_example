3babcf1f31e8153c505becddd2b7beea5e93476c
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2021-08-06 18:16:22 -0700

    using xdg-open

--- configure_project.sh
+++ configure_project.sh
@@ -1,8 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
 git config user.email "culshawmaurer@email.arizona.edu"
 git config user.name "Michael Culshaw-Maurer"
 
 git clone https://github.com/MCMaurer/VICE_Stan_example.git
 
-open VICE_Stan_example/VICE_Stan_example.Rproj
+xdg-open VICE_Stan_example/VICE_Stan_example.Rproj
