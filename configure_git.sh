6df438470b36917e5f34859b222eb4397b4ac29f
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2021-08-07 00:44:12 +0000

    script to configure git

---
+++ configure_git.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+git config user.email "culshawmaurer@email.arizona.edu"
+git config user.name "Michael Culshaw-Maurer"
--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -2,4 +2,6 @@ library(tidyverse)
 
 here::i_am("scripts/clean_data.R")
 
-read_csv(here::here("data/raw/portal_data_joined.csv"))
+d <- read_csv(here::here("data/raw/portal_data_joined.csv"))
+
+d
